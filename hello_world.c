6ee562f2fb77bed9b52c25e1961d20df63cae36a
ZhuYuChenNO1 <944816960@qq.com>
2018-11-09 04:48:16 +0000

    another test

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void println(const char * val);
+
+int main(void) {
+    println("こんにちは 世界");
+}
+
+void println(const char * val) {
+    printf("%s\n");
+    return 0;
+}
